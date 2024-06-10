73014f7e9bf67bea87e666ffcf601fac48e876ee
ivangkaLiyanage <ivangkasevwandhi@gmail.com>
2024-06-10 20:37:29 +0530

    Added the tasks in popup.js

---
+++ popup.js
@@ -0,0 +1,5 @@
+// add eventlistner to generate the button
+// get the length of the password from the input field
+// generate a password with a specified length - password generating logic - call the function
+// display the generated password in the text area
+// creating generate password function - return the generated password
